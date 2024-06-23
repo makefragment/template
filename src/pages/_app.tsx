46ed199aff3780fdeba958e35b64a558ab14a05c
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-06-23 18:44:21 +0330

    feat: add script to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import localFont from "next/font/local";
 import { useEffect } from "react";
-import Script from "next/script";
 
 const iransansFont = localFont({
   src: "../../public/fonts/IRANSansXV.woff2",
@@ -18,14 +17,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     document.body.classList.add(iransansFont.variable);
   }, []);
   return (
-    <>
-      <Script
-        type="text/javascript"
-        src="https://hamdast.paziresh24.com/static/js/scripts/hamdast.js"
-      />
-      <PlasmicRootProvider Head={Head}>
-        <Component {...pageProps} />
-      </PlasmicRootProvider>
-    </>
+    <PlasmicRootProvider Head={Head}>
+      <Component {...pageProps} />
+    </PlasmicRootProvider>
   );
 }

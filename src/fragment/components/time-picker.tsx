dbf7b8df1cf719ee8dd4db63df9a98fd0d8aa3e4
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-07-22 13:38:38 +0330

    feat: add primary color global config

--- src/fragment/components/time-picker.tsx
+++ src/fragment/components/time-picker.tsx
@@ -1,14 +1,34 @@
-import { CodeComponentMeta } from "@plasmicapp/host";
+import { CodeComponentMeta, useSelector } from "@plasmicapp/host";
+import { useEffect } from "react";
 
 import { Timeit } from "react-timeit";
 
-export const TimePicker = ({ onChange, value = "00:00" }: any) => {
+export const TimePicker = ({
+  onChange,
+  value = "00:00",
+  hourExclude,
+  minuteExclude,
+  notShowExclude,
+}: any) => {
+  const fragmentConfig = useSelector("Fragment");
+
+  useEffect(() => {
+    changeTheme(fragmentConfig?.primaryColor);
+  }, [fragmentConfig?.primaryColor]);
+
+  const changeTheme = (color: string) => {
+    document.documentElement.style.setProperty("--timeit-primary-color", color);
+  };
+
   return (
     <Timeit
       onChange={(time) => {
         if (value !== time) onChange(time);
       }}
       defualtValue={value}
+      hourExclude={hourExclude}
+      minuteExclude={minuteExclude}
+      notShowExclude={notShowExclude}
     />
   );
 };
@@ -19,6 +39,17 @@ export const timePickerMeta: CodeComponentMeta<any> = {
   importPath: "@/fragment/components/time-picker",
   props: {
     value: { type: "string", defaultValue: "00:00" },
+    hourExclude: {
+      type: "array",
+      helpText: "[18, 20, 22]",
+    },
+    minuteExclude: {
+      type: "array",
+      helpText: "[15, 30, 25]",
+    },
+    notShowExclude: {
+      type: "boolean",
+    },
     onChange: {
       type: "eventHandler",
       argTypes: [
